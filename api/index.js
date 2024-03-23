395194276345c067625fd21ea0ff28521675fb78
IAMAmanRaj <imamanraj87@gmail.com>
2024-03-23 20:16:25 +0530

    Final Production changes

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
-import cors from "cors"
+import cors from "cors";
 import dotenv from "dotenv";
 import jobRoutes from "./routes/job.route.js";
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+import path from "path";
+
 dotenv.config();
 const port = 3000;
 import authRoutes from "./routes/auth.route.js";
@@ -22,6 +24,12 @@ app.use((err, req, res, next) => {
     message,
     statusCode,
   });
+});
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 mongoose
